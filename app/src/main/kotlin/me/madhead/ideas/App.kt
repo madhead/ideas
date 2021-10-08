186045136984afb2e928756d5f0edf6b26cc2557
madhead <siarhei.krukau@gmail.com>
2021-10-09 01:18:48 +0200

    Render to file instead of stdout

--- app/src/main/kotlin/me/madhead/ideas/App.kt
+++ app/src/main/kotlin/me/madhead/ideas/App.kt
@@ -1,5 +1,8 @@
 package me.madhead.ideas
 
+import java.io.File
+import java.io.PrintWriter
+import kotlin.system.measureTimeMillis
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.filter
@@ -16,7 +19,7 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 
-suspend fun main() {
+suspend fun main(args: Array<String>) {
     startKoin {
         modules(
             envModule,
@@ -26,7 +29,11 @@ suspend fun main() {
         )
     }
 
-    App().run()
+    measureTimeMillis {
+        App().run(args[0])
+    }.let {
+        println("${args[0]} rendered in ${it}ms")
+    }
 }
 
 internal class App : KoinComponent {
@@ -35,29 +42,34 @@ internal class App : KoinComponent {
     private val issuesComparator: IssuesComparator by inject()
     private val render: Markdown by inject()
 
-    suspend fun run() {
-        javaClass.classLoader.getResourceAsStream("header.md").copyTo(System.out)
+    suspend fun run(file: String) {
+        File(file).outputStream().use { output ->
+            val printWriter = PrintWriter(output)
 
-        gitHub.use { gitHub ->
-            gitHub
-                .repos("madhead", "ideas")
-                .issues()
-                .filter(issuesPredicate)
-                .toList()
-                .sortedWith(issuesComparator)
-                .map {
-                    coroutineScope {
-                        async {
-                            render(it)
+            javaClass.classLoader.getResourceAsStream("header.md")!!.copyTo(output)
+
+            gitHub.use { gitHub ->
+                gitHub
+                    .repos("madhead", "ideas")
+                    .issues()
+                    .filter(issuesPredicate)
+                    .toList()
+                    .sortedWith(issuesComparator)
+                    .map {
+                        coroutineScope {
+                            async {
+                                render(it)
+                            }
                         }
                     }
-                }
-                .map { it.await() }
-                .forEach {
-                    println(it)
-                }
-        }
+                    .map { it.await() }
+                    .forEach {
+                        printWriter.println(it)
+                    }
+            }
+            printWriter.flush()
 
-        javaClass.classLoader.getResourceAsStream("footer.md").copyTo(System.out)
+            javaClass.classLoader.getResourceAsStream("footer.md")!!.copyTo(output)
+        }
     }
 }
